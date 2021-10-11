a624dfbaad5b5b10246330a0924b3cd972529f74
mert.cotuk <mert.cotuk@inomera.com>
2021-10-11 22:35:51 +0300

    Initial commit

---
+++ src/main/java/com/bmcotuk/dsaa/DsaaApplication.java
@@ -0,0 +1,13 @@
+package com.bmcotuk.dsaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmcotuk/dsaa/DsaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmcotuk.dsaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
